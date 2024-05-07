5a3756fac542a0c0b36e43819c13c201c1f3685f
flowsar <brahim.sar151@gmail.com>
2024-05-07 23:14:04 +0100

    update

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,13 +1,6 @@
 #!/usr/bin/env ruby
 
-arg=ARGV[0]
 
 pattern = /(h)(b*)(tn)$/
 
-if arg.nil?
-    puts "enter argument"
-    exit
-end
-
-result = arg.scan(pattern)
-puts result
+puts ARGV[0].scan(pattern).join
