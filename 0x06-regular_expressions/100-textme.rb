ffaf0c42327ba181eebe5d3ea3ad223f1e8e3e9a
flowsar <brahim.sar151@gmail.com>
2024-05-08 11:58:11 +0100

    succes

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,1 +1,9 @@
 #!/usr/bin/env ruby
+
+pattern = /(?:from:\w+\S+)|(?:to:\+?\d{11})|(?:flags:-?\d:-?\d:-?\d:-?\d:-?\d)/
+
+argument = ARGV[0]
+
+matches = argument.scan(pattern)
+
+puts "#{matches.join(", ")}"
