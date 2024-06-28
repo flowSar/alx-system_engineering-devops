bb35908a7456d4bd2ba76acf736b65cdd18bdf48
flowsar <brahim.sar151@gmail.com>
2024-06-28 22:23:18 +0100

    up

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 
-exec { 'kill_process':
+# kill process killmenow using pkill
+exec { 'kill_process_killmenow':
   command => '/usr/bin/pkill -f killmenow',
   onlyif  => '/usr/bin/pgrep -f killmenow',
   path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
