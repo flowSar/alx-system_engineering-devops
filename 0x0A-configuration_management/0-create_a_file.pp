97c447b7844a4b1186a890d383dd1f4092546a20
flowsar <brahim.sar151@gmail.com>
2024-06-28 21:19:25 +0100

    puppet file for creating school file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,12 @@
- 
+
+# we creating a school file with "I love puppet content"
+# and 0744 is the permission of the file and thte www-data
+# is the owner and group of the file
+
+file { '/tmp/school'
+    ensure => 'file',
+    content => 'I love Puppet',
+    owner => 'www-data',
+    group => 'www-data'.
+    mode => '0744'
+} 
