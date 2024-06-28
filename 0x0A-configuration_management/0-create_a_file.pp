9d581b4966ce28711eb2e8341c5df5f3bcf4e4d1
flowsar <brahim.sar151@gmail.com>
2024-06-28 21:42:37 +0100

    fix error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,10 +3,10 @@
 # and 0744 is the permission of the file and thte www-data
 # is the owner and group of the file
 
-file { '/tmp/school'
-    ensure => 'file',
-    content => 'I love Puppet',
-    owner => 'www-data',
-    group => 'www-data'.
-    mode => '0744'
-} 
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
