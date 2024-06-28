697e213de75fa753ae7eaeb25b8efe3db889ae37
flowsar <brahim.sar151@gmail.com>
2024-06-28 21:57:22 +0100

    add path

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,7 +7,9 @@ package {'python3-pip':
 # install flask version 2.1.0
 # first we run the unless command to check if flask instlled 
 # before running command
-exec { 'install_flask_specific_version':
+exec { 'install_flask_2.1.0':
   command => '/usr/bin/pip3 install Flask==2.0.1',
+  unless  => '/usr/bin/pip3 show Flask | grep "Version: 2.1.0"',
   require => Package['python3-pip'],
+  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
 }
