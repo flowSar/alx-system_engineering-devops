e2023da1a45300bab47560007a822a49f91c18e6
flowsar <brahim.sar151@gmail.com>
2024-07-29 20:09:46 +0100

    save data to json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""fetch data from API and save it on csv file"""
+import json
+import requests
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+
+    if args:
+        id = args[0]
+
+        todoApiUrl = 'https://jsonplaceholder.typicode.com/todos'
+        userApiUrl = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+        todoTasks = requests.get(url=todoApiUrl).json()
+        userName = requests.get(url=userApiUrl).json()['username']
+
+        tasksNumber = 0
+        task_completed = 0
+
+        data = {id: []}
+        for user in todoTasks:
+            if user['userId'] == int(id):
+                data[id] += [{"task": user['title'],
+                              "completed": user['completed'],
+                              "username": userName}]
+
+        filename = f'{id}.json'
+        with open(filename, mode='w', newline='') as file:
+            json.dump(data, file)
+
+
+if __name__ == "__main__":
+    main()
