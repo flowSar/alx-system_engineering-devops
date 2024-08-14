fea05631288aa98df36235e916570a30646fec14
flowsar <brahim.sar151@gmail.com>
2024-08-14 14:31:58 +0100

    up

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """fetch and print 10 hot posts on reddit using reddit API"""
     params = {
-        'limit': 9,
+        'limit': 8,
     }
     api_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     response = requests.get(api_url, allow_redirects=False, params=params)
