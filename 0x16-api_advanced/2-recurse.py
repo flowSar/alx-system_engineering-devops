de0a10419b534895110cae8b042459eea37ab863
flowsar <brahim.sar151@gmail.com>
2024-08-14 13:19:55 +0100

    fetch all host posts recuersively

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,28 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+"""fetch hot posts from reddit API"""
+import requests
+
+params = {
+    'limit': 100,
+    'count': 0,
+    'after': None
+}
+
+
+def recurse(subreddit, hot_list=[]):
+    """fetch all hot posts"""
+
+    api_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    response = requests.get(api_url, params=params)
+    if response.status_code != 200:
+        return hot_list
+
+    data = response.json()
+    posts = data['data']['children']
+    for post in posts:
+        post_data = post['data']
+        title = post_data['title']
+        hot_list.append(title)
+        print(title)
+
+    recurse(subreddit, hot_list)
