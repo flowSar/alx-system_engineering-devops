2e7afbe30aaf72f86de91e8e1e56c8191b144e51
flowsar <brahim.sar151@gmail.com>
2024-08-14 11:38:51 +0100

    fetch reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,15 @@
- 
+#!/usr/bin/python3
+"""fetch reddit API and get number os subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return number of subscribers for each subreddit
+    or 0 if the subreddit if not found"""
+    url_api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url_api)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
