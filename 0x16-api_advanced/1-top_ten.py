3d0309c9e377d3ba0f159c209f3aa318c81af124
flowsar <brahim.sar151@gmail.com>
2024-08-14 12:35:59 +0100

    fetch 10 hot posts on reddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+"""fetch hot posts from reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """fetch and print 10 hot posts on reddit using reddit API"""
+    api_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for index in range(10):
+            post = posts[index]
+            post_data = post['data']
+            title = post_data['title']
+            print(title)
